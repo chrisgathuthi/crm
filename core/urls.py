9f76394935f9fb8dab3e544641ac087e26314c32
Saint-Chris-7 <chrisgathuthi@gmail.com>
2024-01-16 16:25:06 +0300

    doc - doc endpoint

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -26,4 +28,6 @@ urlpatterns = [
     path("api/accounts/", include("accounts.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("schema/",SpectacularAPIView.as_view(),name="schema"),
+    path("docs/",SpectacularSwaggerView.as_view(url_name="schema"),name="docs")
 ]
