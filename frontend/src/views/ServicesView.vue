0517d00c01270a663d7678846dec6efb414512bf
Saint-Chris-7 <chrisgathuthi@gmail.com>
2024-03-10 21:52:37 +0300

    ui - change layout

--- frontend/src/views/ServicesView.vue
+++ frontend/src/views/ServicesView.vue
@@ -5,16 +5,22 @@ import Navigation from '../components/Navigation.vue';
 const router = useRouter()
 </script>
 <template>
-    <aside class="bg-pink">
-        <Navigation />
-    </aside>
-    <main class="h-screen">
-        <v-toolbar class="bg-grey-lighten-5 border-b">
-            <v-toolbar-title>Faiba plans</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn @click="router.push({ name: 'service-form' })">faiba plans</v-btn>
-        </v-toolbar>
+    <main class="">
+        <aside class="bg-red">
+            <Navigation />
+        </aside>
+        <section>
+            <div>
+                <v-app-bar prominent class="bg-grey-lighten-5" tag="div">
+                    <v-toolbar-title>Faiba plans</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn @click="router.push({ name: 'service-form' })">faiba plans</v-btn>
+                </v-app-bar>
 
-        <RouterView />
+            </div>
+
+
+            <RouterView />
+        </section>
     </main>
 </template>
